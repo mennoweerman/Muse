e87f5f81b10137a88268a4b9d5c9eaeae10c3b9b
George Fritz <30572@ma-web.nl>
2022-06-23 10:46:28 +0200

    Total UI Script update
    -UI script op DRY manier geschreven
    -Tags
    -UI update

--- MuseF2M8/Assets/Scripts/Respawn.cs
+++ MuseF2M8/Assets/Scripts/Respawn.cs
@@ -1,14 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Video;
 
 public class Respawn : MonoBehaviour
 {
     public Vector3 startPos;
-    public GameObject UI;
-    public float deathUITimer;
-    [HideInInspector] public VideoPlayer videoPlayer;
 
     private void Start()
     {
@@ -16,30 +10,9 @@ public class Respawn : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     public void RespawnPlayer()
     {
         transform.position = startPos;
-        UI.SetActive(true);
-        Debug.Log(transform.position);
-        //gameObject.transform = startPos;
-        StartCoroutine(DeathUITimer());
     }
-
-    private IEnumerator DeathUITimer()
-    {
-        yield return new WaitForSeconds(deathUITimer);
-        UI.SetActive(false);
-        FindObjectOfType<UIScript>().StartTimer(true);
-    }
-
-    public void DisableVideoPlayer()
-    {
-        videoPlayer.gameObject.SetActive(false);
-    }
-
 }
